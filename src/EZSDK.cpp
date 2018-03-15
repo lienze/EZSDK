9a45c637488c6fec780df17397fbe8e3007ba249
lienze <lienze2010@126.com>
2018-03-15 15:41:05 +0800

    using EZ namespace

--- src/EZSDK.cpp
+++ src/EZSDK.cpp
@@ -6,10 +6,12 @@
 #include "EZLog.h"
 #include "EZDef.h"
 
-EZ::EZLogMan g_LogMan;
+using namespace EZ;
+
+EZLogMan g_LogMan;
 
 void TestEZReadFile() {
-	EZ::EZReadFile fFile;
+	EZReadFile fFile;
 	int iRet = fFile.FileOpen("./test.txt");
 	if (iRet == 1)
 	{
@@ -29,7 +31,7 @@ void TestEZReadFile() {
 }
 
 void TestEZSaveFile(){
-	EZ::EZSaveFile fFile;
+	EZSaveFile fFile;
 	int iRet = fFile.FileOpen("./test.txt");
 	if (iRet == 1)
 	{
@@ -40,20 +42,20 @@ void TestEZSaveFile(){
 }
 
 void TestEZRemoveFile() {
-	EZ::EZFileBase::CreateFile("./test.txt");
-	EZ::EZFileBase::RemoveFile("./test.txt");
+	EZFileBase::CreateFile("./test.txt");
+	EZFileBase::RemoveFile("./test.txt");
 }
 
 void TestEZCreateFile(){
-	EZ::EZFileBase::CreateFile("./aaa.txt");
+	EZFileBase::CreateFile("./aaa.txt");
 }
 
 void TestEZString(){
-	EZ::EZString szStr;
+	EZString szStr;
 	szStr = "Hello";
 	std::cout << szStr.content() << std::endl;
 
-	EZ::EZStringList szStrList;
+	EZStringList szStrList;
 	std::string szText = "Hello";
 	szStrList.AddInTail(szText);
 	std::string szText1 = "Hello1";
@@ -67,23 +69,23 @@ void TestEZString(){
 }
 
 void TestEZStringWithOperator(){
-	EZ::EZString szStr;
+	EZString szStr;
 	szStr = "Hello";
 	printf("%s\n",szStr["0:3"].content().c_str());
 }
 
 void TestEZStringWithOperatorPlus(){
-	EZ::EZString szStr1,szStr2;
+	EZString szStr1,szStr2;
 	szStr1 = "Good";
 	szStr2 = "Well";
-	EZ::EZString res = szStr1 + szStr2;
+	EZString res = szStr1 + szStr2;
 	printf("%s\n",res.content().c_str());
 }
 
 void TestEZStringSplitFunc(){
 	std::string szText = "abc,1,2,999";
-	EZ::EZString ez_string = szText;
-	EZ::EZStringList ez_str_list = ez_string.split(',');
+	EZString ez_string = szText;
+	EZStringList ez_str_list = ez_string.split(',');
 	//std::cout << ez_str_list[0]->content();
 	printf("%s %s %s %s\n",
 		ez_str_list[0]->content().c_str(),
@@ -94,15 +96,15 @@ void TestEZStringSplitFunc(){
 
 void TestEZStringListInsert(){
 	std::string szText = "abc,1,2";
-	EZ::EZString ez_string = szText;
-	EZ::EZStringList ez_str_list = ez_string.split(',');
+	EZString ez_string = szText;
+	EZStringList ez_str_list = ez_string.split(',');
 	std::string ss = "aaa";
 	ez_str_list.InsertAt(0,ss);
 	ez_str_list.PrintList();
 }
 
 void TestEZTime(){
-	EZ::EZTime _time;
+	EZTime _time;
 	std::cout << _time.GetYear() << std::endl;
 	std::cout << _time.GetMonth() << std::endl;
 	std::cout << _time.GetDay() << std::endl;
@@ -115,11 +117,11 @@ void TestEZTime(){
 // Funcionts for Client
 //////////////////////////////////////////////////
 void TestEZNetClient(){
-	EZ::EZUDP aUDP;
-	EZ::EZUDP bUDP;
+	EZUDP aUDP;
+	EZUDP bUDP;
 	aUDP.InitNetSend("192.168.199.101",8888);
 	aUDP.SendTo("Hello");
-	EZ::EZNetMan g_NetMan;
+	EZNetMan g_NetMan;
 	g_NetMan.AddUnit(&aUDP);
 	g_NetMan.AddUnit(&bUDP);
 	int _pos=0;
@@ -131,13 +133,13 @@ void TestEZNetClient(){
 // Funcionts for Server
 //////////////////////////////////////////////////
 void Test_InitLogForServer(){
-	EZ::EZLog *sysLog = new EZ::EZLog;
+	EZLog *sysLog = new EZLog;
 	if(sysLog)
 		sysLog->InitLog("./Log/sys.txt");
-	EZ::EZLog *clientLog = new EZ::EZLog;
+	EZLog *clientLog = new EZLog;
 	if(clientLog)
 		clientLog->InitLog("./Log/client.txt");
-	EZ::EZLog *errLog = new EZ::EZLog;
+	EZLog *errLog = new EZLog;
 	if(errLog)
 		errLog->InitLog("./Log/err.txt");
 	g_LogMan.AddLogger(sysLog,SYSLOG);
@@ -146,7 +148,7 @@ void Test_InitLogForServer(){
 }
 
 void TestEZNetServer(){
-	EZ::EZUDP aUDP;
+	EZUDP aUDP;
 	aUDP.InitNetRecv(8888);
 	Test_InitLogForServer();
 	ELOG(ERRORLOG,"Hello World!");
