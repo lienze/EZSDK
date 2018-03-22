04ead158cefca958ae4f85bef1d98fe75cedb729
lienze <lienze2010@126.com>
2018-03-22 21:29:48 +0800

    增加网络模型的宏定义

--- src/EZDef.h
+++ src/EZDef.h
@@ -2,3 +2,8 @@
 #define ERRORLOG  EZ::LogType::ERR
 #define CLIENTLOG EZ::LogType::CLIENT
 #define SYSLOG    EZ::LogType::SYS
+
+//define for EZNet Module
+#define MICROSECONDS	1000000				//1s
+#define MICROSECONDS_2	(MICROSECONDS/2)	//0.5s
+#define MICROSECONDS_4	(MICROSECONDS/4)	//0.25s
