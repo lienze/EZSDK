587b406d8bcb7769d7b325576679a005b617db5b
lienze <lienze2010@126.com>
2018-12-28 16:29:07 +0800

    重构EZTime模块

---
+++ src/EZTime.cpp
@@ -0,0 +1,43 @@
+#include "EZTime.h"
+
+namespace EZ {
+
+	EZTime::EZTime() {
+		t_time = NULL;
+	}
+	EZTime::~EZTime() {}
+	bool EZTime::UpdateTime() {
+		time(&raw_time);
+		t_time = localtime(&raw_time);
+		return true;
+	}
+	std::tm *EZTime::GetTime() {
+		if (t_time == NULL) {
+			UpdateTime();
+		}
+		return t_time;
+	}
+	int EZTime::GetYear() {
+		return GetTime()->tm_year + 1900;
+	}
+	int EZTime::GetMonth() {
+		return GetTime()->tm_mon + 1;
+	}
+	int EZTime::GetDay() {
+		return GetTime()->tm_mday;
+	}
+	int EZTime::GetWeekDay() {
+		int iWeekDayTmp = 0;
+		return (iWeekDayTmp = GetTime()->tm_wday) == 0 ? 7 : iWeekDayTmp;
+	}
+	int EZTime::GetHour() {
+		return GetTime()->tm_hour;
+	}
+	int EZTime::GetMinutes() {
+		return GetTime()->tm_min;
+	}
+	int EZTime::GetSeconds() {
+		return GetTime()->tm_sec;
+	}
+
+}//end namespace EZ
--- src/EZTime.h
+++ src/EZTime.h
@@ -7,43 +7,17 @@ namespace EZ {
 class EZTime
 {
 public:
-	EZTime(){
-		t_time = NULL;
-	}
-	~EZTime(){}
-	bool UpdateTime(){
-		time(&raw_time);
-		t_time = localtime(&raw_time);
-		return true;
-	}
-	std::tm *GetTime(){
-		if(t_time==NULL){
-			UpdateTime();
-		}
-		return t_time;
-	}
-	int GetYear(){
-		return GetTime()->tm_year + 1900;
-	}
-	int GetMonth(){
-		return GetTime()->tm_mon + 1;
-	}
-	int GetDay(){
-		return GetTime()->tm_mday;
-	}
-	int GetWeekDay(){
-		int iWeekDayTmp = 0;
-		return (iWeekDayTmp = GetTime()->tm_wday)==0?7:iWeekDayTmp;
-	}
-	int GetHour(){
-		return GetTime()->tm_hour;
-	}
-	int GetMinutes(){
-		return GetTime()->tm_min;
-	}
-	int GetSeconds(){
-		return GetTime()->tm_sec;
-	}
+	EZTime();
+	~EZTime();
+	bool UpdateTime();
+	std::tm *GetTime();
+	int GetYear();
+	int GetMonth();
+	int GetDay();
+	int GetWeekDay();
+	int GetHour();
+	int GetMinutes();
+	int GetSeconds();
 private:
 	time_t raw_time;
 	struct std::tm *t_time;
