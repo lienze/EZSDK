3372cc485a4f1dff4a7f9d3f07092f7069ba3275
Enze Li <lienze2010@126.com>
2019-08-30 17:15:06 +0800

    added a script which build the cscopes and tags

---
+++ bin/rebuildtags.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd ../
+path=`pwd`
+echo "rebuild path:"$path
+files=$(ls $path)
+for filename in $files
+do
+	if [[ ${filename:0:6} -eq "cscope" ]];then
+		rm cscope*
+		break
+	fi
+done
+
+if [ -f tags ];then
+	rm tags
+fi
+ctags -R
+cscope -Rbqk
+echo "rebuild succeed!"
