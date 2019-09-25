c24c05c63145575a95ffcd90dce73f3eb373ac2a
Enze Li <lienze2010@126.com>
2019-09-25 08:56:34 +0800

    rename rebuildtags.sh

--- bin/buildtags.sh
+++ bin/buildtags.sh
@@ -5,15 +5,19 @@ echo "rebuild path:"$path
 files=$(ls $path)
 for filename in $files
 do
-	if [[ ${filename:0:6} -eq "cscope" ]];then
+	if [[ ${filename:0:6} == "cscope" ]];then
 		rm cscope*
+		echo "remove cscope files..."
 		break
 	fi
 done
 
 if [ -f tags ];then
 	rm tags
+	echo "remove tags..."
 fi
 ctags -R
+echo "build tags..."
 cscope -Rbqk
-echo "rebuild succeed!"
+echo "build cscope..."
+echo "build succeed!"
